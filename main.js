dbc0df41e648b4043038a9bacb4c9ecedaa06d26
Abduloh <abdullohjonirgashev@gmail.com>
2021-01-14 12:03:57 +0500

    best-repo

---
+++ main.js
@@ -0,0 +1,12 @@
+var ism = prompt("Ismizni kiriting ");
+var yosh = prompt("Yoshingiz nechida");
+if (ism == Azamat) {
+  console.log("Kirish mumkin");
+} else {
+  console.log("mumkin emas");
+}
+if (yosh >= 18) {
+  console.log("kirish mumkin");
+} else {
+  console.log("Man etiladi");
+}
